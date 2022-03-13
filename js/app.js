faa3cdc6dde215f025f55e66499f94e6cef05656
AhmedAdel19 <adel.eng20@gmail.com>
2022-03-13 15:33:06 +0200

    landing_page_commit

---
+++ js/app.js
@@ -0,0 +1,180 @@
+/**
+ * 
+ * Manipulating the DOM exercise.
+ * Exercise programmatically builds navigation,
+ * scrolls to anchors from navigation,
+ * and highlights section in viewport upon scrolling.
+ * 
+ * Dependencies: None
+ * 
+ * JS Version: ES2015/ES6
+ * 
+ * JS Standard: ESlint
+ * 
+*/
+
+/**
+ * Comments should be present at the beginning of each procedure and class.
+ * Great to have comments before crucial code sections within the procedure.
+*/
+
+/**
+ * Define Global Variables
+ * 
+*/
+
+var navContainer = document.getElementById('navbar__list');
+var navList = navContainer.querySelectorAll('.navSec');
+var mainSection = document.getElementsByTagName('main')[0];
+var sections = document.querySelectorAll('section');
+addOnClickEventToNavSection(navList, sections);
+/**
+ * End Global Variables
+ * Start Helper Functions
+ * 
+*/
+
+//Start funaction that check if the section in view port or not
+function isSectionInViewPort(section) {
+    let section_rect = section.getBoundingClientRect();
+    if (section_rect.left >= 0
+        && section_rect.height >= 10
+        && section_rect.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+        && section_rect.right <= (window.innerWidth || document.documentElement.clientWidth)) {
+        return true;
+
+    } else {
+        return false
+    }
+}
+//End funaction that check if the section in view port or not
+
+
+//Start function that return the section num
+function sectionNumber(section) {
+    return section.id.charAt(section.id.length - 1);
+}
+//End function that return the section num
+
+
+//start function that Remove Previous Active cLasses from section list
+function removeActiveClasses(SectionList) {
+    SectionList.forEach(function (t) {
+        if (t.classList.contains("active")) {
+            t.classList.remove('active');
+        }
+    })
+}
+//End function that Remove Previous Active cLasses from section list
+
+
+
+
+/**
+ * End Helper Functions
+ * Begin Main Functions
+ * 
+*/
+
+// build the nav
+// Build menu
+//Start function that add new section to the nav section and main section
+function AddNewSection() {
+    let newSectionNumber = navList.length + 1;
+    //create new nav element
+    let newElm = document.createElement('li');
+    newElm.id = "navSection" + newSectionNumber;
+    newElm.className = 'navSec';
+
+    //create new section element
+    let newSec = document.createElement('section');
+    newSec.id = "section" + newSectionNumber;
+    newSec.setAttribute("data-nav", "Section " + newSectionNumber);
+
+    //create div inside new section
+    let sectionDiv = document.createElement('div');
+    sectionDiv.className = "landing__container";
+
+
+    let sectionName = prompt("Please enter section name", "Section " + newSectionNumber);
+    if (sectionName != null) {
+        let sectionContent = prompt("Please enter section content", "Section content of section no :  " + newSectionNumber);
+        if (sectionContent != null) {
+
+            newElm.innerHTML = sectionName;
+
+            let sectionTitle = document.createElement('h2');
+            sectionTitle.innerHTML = sectionName;
+            let sectionp = document.createElement('p');
+            sectionp.innerHTML = sectionContent;
+
+            sectionDiv.appendChild(sectionTitle);
+            sectionDiv.appendChild(sectionp);
+            newSec.appendChild(sectionDiv);
+            mainSection.appendChild(newSec);
+
+            let btnAdd = document.getElementById("addBtn");
+            btnAdd.insertAdjacentElement("beforebegin", newElm);
+
+            //navContainer.in.appendChild(newElm);
+
+            navContainer = document.getElementById('navbar__list');
+            navList = navContainer.querySelectorAll('.navSec');
+            mainSection = document.getElementsByTagName('main')[0];
+            sections = document.querySelectorAll('section');
+
+            addOnClickEventToNavSection(navList, sections);
+        } else {
+            alert("section content can't be empty!");
+        }
+    } else {
+        alert("section name can't be empty!");
+    }
+}
+//End function that add new section to the nav section and main section 
+
+
+//Start to add click event to nave sections
+
+function clicklSectionListener(s) {
+    setTimeout(() => {
+        let activeNavs = navContainer.getElementsByClassName("active");
+        activeNavs[0].classList.remove("active");
+
+        // Set sections as active
+        this.className = this.className + " active";
+        // Scroll to section on link click
+        // Scroll to anchor ID using scrollTO event
+        s.scrollIntoView(true);
+    }, 0);
+
+}
+
+function addOnClickEventToNavSection(navList, sections) {
+    for (let i = 0; i < navList.length; i++) {
+        navList[i].addEventListener('click', () => clicklSectionListener(sections[i]))
+    }
+}
+//End to add click event to nave sections
+
+/**
+ * End Main Functions
+ * Begin Events
+ *
+*/
+
+//scroll event 
+document.addEventListener('scroll', function () {
+    sections.forEach(function (s) {
+        if (isSectionInViewPort(s)) {
+            let sectionNum = sectionNumber(s);
+            removeActiveClasses(navList);
+            navList[sectionNum - 1].classList.add('active');
+        }
+
+    })
+})
+
+
+
+
